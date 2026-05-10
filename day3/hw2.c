bcd9c69e889fdb5514f68a63dec9fd4a9197a3db
Denel-20 <dcsbsc02@gmail.com>
2026-05-10 07:40:22 +0000

    과제2 완

--- day3/hw2.c
+++ day3/hw2.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 struct Data {
-    int *val;
-    int *val2;
-    int *val3;
+    int val;
+    int val2;
+    int val3;
 };
 
+void swap(struct Data *A, struct Data *B) {
+    struct Data c = *A;
+    *A = *B;
+    *B = c;
+}
+
 int main() {
-    int a = 123;
-    int b = 231;
-    int c = 312;
+    struct Data a = {1, 2, 3};
+    struct Data b = {3, 2, 1};
+    
+    printf("a: %d %d %d\n", a.val, a.val2, a.val3);
+    printf("b: %d %d %d\n", b.val, b.val2, b.val3);
+
+    swap(&a, &b);
+
+    printf("a: %d %d %d\n", a.val, a.val2, a.val3);
+    printf("b: %d %d %d\n", b.val, b.val2, b.val3);
 
     return 0;
 }
